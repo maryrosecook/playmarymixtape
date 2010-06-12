070e42e97aac7dd47506aa9ade83158878f824d0
maryrosecook <maryrosecook@maryrosecook.com>
2010-06-12 09:50:22 +0200

    track html auto importer now takes track titles and artists from the actualy mp3info

--- script/mp3_dir_to_sql.rb
+++ script/mp3_dir_to_sql.rb
@@ -4,6 +4,9 @@
 # e.g.
 #   ruby mp3_dir_to_sql.rb "../../../Desktop/mp3s/" 606
 
+require 'rubygems'
+require 'mp3info'
+
 def esc_speech_and_apos(str)
   str = str.gsub(/"/, '\"')
   return_str = str
@@ -32,9 +35,22 @@ else
     url = base_url + esc_speech_and_apos(filename)
     title = esc_speech_and_apos(filename.gsub(/\.mp3/, ""))
     artist = esc_speech_and_apos(filename.gsub(/\.mp3/, ""))
+    
+    # try and extract real artist/name info
+    begin
+      Mp3Info.open(filename) do |mp3|
+        title = mp3.tag.title
+        artist = mp3.tag.artist_name
+        artist = mp3.tag.artist if !artist # try again with .artist
+        artist = mp3.tag2['TP1'] if !artist # try again w/ tag2
+      end
+    rescue
+      print "woo"
+    end
+
     created_at = time_to_sql_time(Time.now)
     updated_at = time_to_sql_time(Time.now)
-    permalink = artist.downcase.gsub(/\W/, "").gsub(/\d/, "")
+    permalink = (artist.downcase + "_" + title.downcase).gsub(/\W/, "")
     sort_order = i
   
     sql = "insert into tracks (url, title, artist, comment, audiography_id, created_at, updated_at, permalink, sort_order) VALUES ('#{url}', '#{title}', '#{artist}', '#{comment}', #{audiography_id}, '#{created_at}', '#{updated_at}', '#{permalink}', #{sort_order});"
